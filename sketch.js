12db79c3829afa63920150cf1173a63a80f1ec07
LoveArtHate <artist@lovearthate.com>
2019-07-22 13:52:57 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1040, 1080);
+  createCanvas(500, 500);
 }
 
 function draw() {
@@ -9,27 +9,48 @@ function draw() {
     fill(255);
   }
   ellipse(mouseX, mouseY, 30, 80);
+};
+let message = 'hover over the artwork here*',
+  font,
+  bounds, // holds x, y, w, h of the text's bounding box
+  fontsize = 60,
+  x,
+  y; // x and y coordinates of the text
+
+function preload() {
+  font = loadFont('assets/SourceSansPro-Regular.otf');
 }
-let a = 0; // Create a global variable "a" of type Number
-let b = 0; // Create a global variable "b" of type Number
 
 function setup() {
-  createCanvas(720, 400);
-  stroke(255);
+  createCanvas(710, 400);
+
+  // set up the font
+  textFont(font);
+  textSize(fontsize);
+
+  // get the width and height of the text so we can center it initially
+  bounds = font.textBounds(message, 0, 0, fontsize);
+  x = width / 2 - bounds.w / 2;
+  y = height / 2 - bounds.h / 2;
 }
 
 function draw() {
-  background(0);
+  background(204, 120);
 
-  a = a + 1; // Increment a with an integer
-  b = b + 2.2; //Increment b with a float
-  line(a, 0, a, height / 2);
-  line(b, height / 2, b, height);
+  // write the text in black and get its bounding box
+  fill(0);
+  text(message, x, y);
+  bounds = font.textBounds(message, x, y, fontsize);
 
-  if (a > width) {
-    a = 0;
-  }
-  if (b > width) {
-    b = 0;
+  // check if the mouse is inside the bounding box and tickle if so
+  if (
+    mouseX >= bounds.x &&
+    mouseX <= bounds.x + bounds.w &&
+    mouseY >= bounds.y &&
+    mouseY <= bounds.y + bounds.h
+  ) {
+    x += random(-5, 20);
+    y += random(-5, 9);
   }
 }
+
